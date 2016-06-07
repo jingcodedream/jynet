87780116d6c9e0406191937f922fe92e1a2b2a81
jingcodedream <767302105@qq.com>
2016-06-07 19:45:09 +0800

    修改文件名

---
+++ src/core/io_server/epoll_io_server.h
@@ -0,0 +1,36 @@
+/*
+ * epoll_io_server.h
+ *
+ *  Created on: 2016年6月7日
+ *      Author: joe
+ */
+
+#ifndef SRC_CORE_IO_SERVER_EPOLL_IO_SERVER_H_
+#define SRC_CORE_IO_SERVER_EPOLL_IO_SERVER_H_
+
+
+
+class IOServerEpoll : public IOServer
+{
+public:
+    IOServerEpoll();
+    IOServerEpoll(uint32_t flags, uint32_t maxevents, uint32_t timeout);
+    ~IOServerEpoll();
+
+    bool RunOnce();
+    void RunForever();
+
+    int AddEvent(uint32_t op, uint32_t fd, Session *session);
+    int DelEvent(uint32_t events, uint32_t fd);
+    int WaitEvent();
+
+private:
+    int epfd;
+    struct epoll_event *events;
+    uint32_t maxevents;
+    uint32_t timeout;
+};
+
+
+
+#endif /* SRC_CORE_IO_SERVER_EPOLL_IO_SERVER_H_ */
---
+++ src/core/io_server/io_server.h
@@ -0,0 +1,40 @@
+/*
+ * IOServer.h
+ *
+ *  Created on: 2016年5月11日
+ *      Author: joe
+ */
+
+#ifndef SRC_CORE_IO_SERVER_H_
+#define SRC_CORE_IO_SERVER_H_
+
+#include <stdint.h>
+
+const uint32_t io_empty = 0x0000;
+const uint32_t io_read  = 0x0001;
+const uint32_t io_write = 0x0002;
+const uint32_t io_rdwt  = 0x0003;
+const uint32_t io_error = 0x0004;
+
+class Session;
+
+typedef struct EventInfo
+{
+    int fd;
+    Session *session;
+}eventinfo;
+
+class IOServer
+{
+public:
+    virtual ~IOServer(){}
+
+    virtual bool RunOnce() = 0;
+    virtual void RunForever() = 0;
+
+    virtual int AddEvent(uint32_t op, uint32_t fd, Session *session) = 0;
+    virtual int DelEvent(uint32_t events, uint32_t fd) = 0;
+    virtual int WaitEvent() = 0;
+};
+
+#endif /* SRC_CORE_IOSERVER_H_ */
